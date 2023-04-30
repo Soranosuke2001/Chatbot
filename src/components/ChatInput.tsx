e6d4c14c8cce5cd6eedb7adc96299348bccc52ce
Soranosuke2001 <sorazora1.4@gmail.com>
2023-04-30 12:48:56 -0700

    chatbot support box outline

---
+++ src/components/ChatInput.tsx
@@ -0,0 +1,14 @@
+import { FC, HTMLAttributes } from "react";
+import { cn } from "../../lib/utils";
+
+interface ChatInputProps extends HTMLAttributes<HTMLDivElement> {}
+
+const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
+  return (
+    <div {...props} className={cn("border-t border-zinc-300", className)}>
+      ChatInput
+    </div>
+  );
+};
+
+export default ChatInput;
