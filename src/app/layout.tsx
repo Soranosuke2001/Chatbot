39a75645ec6ec715fe1f22e4b574833f62cc4a2d
Soranosuke2001 <sorazora1.4@gmail.com>
2023-04-30 13:08:24 -0700

    added react-query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,28 @@
-import Chat from '@/components/Chat'
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Chat from "@/components/Chat";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Providers from "@/components/Providers";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'BookNext',
-  description: 'The bookstore to find fantasy and mystery novels!',
-}
+  title: "BookNext",
+  description: "The bookstore to find fantasy and mystery novels!",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Chat />
-        {children}
-      </body>
+      <Providers>
+        <body className={inter.className}>
+          <Chat />
+          {children}
+        </body>
+      </Providers>
     </html>
-  )
+  );
 }
