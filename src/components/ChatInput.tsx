7cf1f7fae76051f0f9c701e5ca3e69780dbd187c
Soranosuke2001 <sorazora1.4@gmail.com>
2023-04-30 13:22:20 -0700

    user input validator

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -4,11 +4,31 @@ import { FC, HTMLAttributes, useState } from "react";
 import { cn } from "../../lib/utils";
 
 import TextareaAutosize from "react-textarea-autosize";
+import { useMutation } from "@tanstack/react-query";
+import { nanoid } from "nanoid";
+import { Message } from "../../lib/validators/Message";
 
 interface ChatInputProps extends HTMLAttributes<HTMLDivElement> {}
 
 const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
   const [input, setInput] = useState<string>("");
+
+  const { mutate: sendMessage, isLoading } = useMutation({
+    mutationFn: async (message: Message) => {
+      const response = await fetch("/api/message", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ messages: "Hello" }),
+      });
+
+      return response.json();
+    },
+    onSuccess: () => {
+      console.log("Completed successfully");
+    },
+  });
   return (
     <div {...props} className={cn("border-t border-zinc-300", className)}>
       <div className="relative mt-4 flex-1 overflow-hidden rounded-lg border-none outline-non">
@@ -18,6 +38,19 @@ const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
           autoFocus
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && !e.shiftKey) {
+              e.preventDefault();
+
+              const message = {
+                id: nanoid(),
+                isUserMessage: true,
+                text: input
+              }
+              sendMessage(message);
+              setInput("");
+            }
+          }}
           placeholder="Ask a question..."
           className="peer disabled:opacity-50 pr-14 resize-none block w-full border-0 bg-zinc-100 py-1.5 text-gray-900 focus:ring-0 text-sm sm:leading-6"
         />
